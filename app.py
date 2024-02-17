c00b155dc6a179347f8cdd7506b0536ce9d1638d
dropella-dev <156456149+dropella-dev@users.noreply.github.com>
2024-02-17 18:04:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 import os
-from selenium import webdriver
+import undetected_chromedriver as  webdriver
 import re
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
